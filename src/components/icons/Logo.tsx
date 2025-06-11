f47bbe8741987c94c055c4bfbe4735ce3a1ecf4c
toxic toad <toxictoad111@gmail.com>
2025-06-11 14:09:42 +0000

    why isnt gamblr nation not fully visible anywhere?

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,9 +1,10 @@
+
 import type { SVGProps } from 'react';
 
 const Logo = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 200 50"
+    viewBox="0 0 240 50"
     aria-label="GamblrNation Hub Logo"
     role="img"
     {...props}
